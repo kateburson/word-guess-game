ed0ab3c5493abc8d7f1ba4a2e89846029b355116
Edward Hopper <burson.kate@gmail.com>
2018-12-30 17:49:09 -0800

    for loop not the right strategy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 window.onload = function() {
     var start = document.getElementById('start');
     var div = document.createElement('div');
@@ -6,19 +5,16 @@ window.onload = function() {
     start.appendChild(div);
 };
 
-
-document.onkeyup = function() {
-
-var chapstick = ['c' + 'h' + 'a' + 'p' + 's' + 't' + 'i' + 'c' + 'k'];
-var quesedilla = ['q', 'u', 'e', 's', 'e', 'd', 'i', 'l', 'l', 'a'];
-var pigskin = ['p', 'i', 'g', 's', 'k', 'i', 'n'];
-var liger = ['l', 'i', 'g', 'e', 'r'];
-var skills = ['s', 'k', 'i', 'l', 'l', 's'];
-var talons = ['t', 'a', 'l', 'o', 'n', 's'];
-var dreams = ['d', 'r', 'e', 'a', 'm', 's'];
-var babes = ['b', 'a', 'b', 'e', 's'];
-var college = ['c', 'o', 'l', 'l', 'e', 'g', 'e'];
-var tots = ['t' + 'o' + 't' + 's'];
+// var chapstick = ['c' , 'h' , 'a' , 'p' , 's' , 't' , 'i' , 'c' , 'k'];
+// var quesedilla = ['q', 'u', 'e', 's', 'e', 'd', 'i', 'l', 'l', 'a'];
+// var pigskin = ['p', 'i', 'g', 's', 'k', 'i', 'n'];
+// var liger = ['l', 'i', 'g', 'e', 'r'];
+// var skills = ['s', 'k', 'i', 'l', 'l', 's'];
+// var talons = ['t', 'a', 'l', 'o', 'n', 's'];
+// var dreams = ['d', 'r', 'e', 'a', 'm', 's'];
+// var babes = ['b', 'a', 'b', 'e', 's'];
+// var college = ['c', 'o', 'l', 'l', 'e', 'g', 'e'];
+// var tots = ['t' + 'o' + 't' + 's'];
 
 var wordBank = [
     chapstick, 
@@ -33,35 +29,47 @@ var wordBank = [
     tots
 ];
 
+var randomWord = "";
+var lettersOfWord = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongGuess = [];
 
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
 
+document.onkeydown = function() {
 
-    for(var i = 0; i < wordBank.length; i++) {
-        var word = document.getElementById('word');
-        var nextWord = wordBank[i];
-        var div = document.createElement('div');
-        div.textContent= nextWord;
-        word.appendChild(div);
+    if (start.style.display === "none") {
+        start.style.display = "none";
+    } else {
+        start.style.display = "none";
+    }
+    
+    function game() {
+        randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+        lettersOfWord = randomWord.split("");
+        blanks = lettersOfWord.length;
+        for (var i = 0; i < blanks; i++) {
+            blanksAndCorrect.push("_");
+        }
     }
 
+    // for(i = 0, i < wordBank.length, i++) {
+    //     var word = document.getElementById('word');
+    //     var nextWord = wordBank[i];
+    //     var ul = document.createElement('ul');
+    //     ul.textContent = nextWord.join('').replace(/[a-z]/g, '_');
+    //     word.appendChild(ul);
+    //     var userGuess = element.key;
+    //         if(userGuess.onkeyup) {
+    //             ul.textContent = nextWord.join('');
+    //             word.appendChild(ul);
+    //         };
+    // };
+
+    
     
 
-    // for(var i=0; i < chapstick.length; i++) {
-    //     var letter = document.getElementById('letter');
-    //     var nextLetter = chapstick[i];
-    //     var li = document.createElement('li');
-    //     li.textContent(letter);
-    //     letter.appendChild(li);
-    // }
-
-
-
-
-        // if(userGuess = letter) {
-        //     element.display(letter)
-        // }
-
-
-
-
-};
+}
