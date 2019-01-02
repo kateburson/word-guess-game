917a60a72a632f54441251cbd5e427a33daaa666
Edward Hopper <burson.kate@gmail.com>
2019-01-01 17:33:49 -0800

    tried to add a key listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,15 @@
-window.onload = function() {
-    var start = document.getElementById('start');
-    var div = document.createElement('div');
-    div.textContent= "Press a flippin' key to start.";
-    start.appendChild(div);
-};
-
-// var chapstick = ['c' , 'h' , 'a' , 'p' , 's' , 't' , 'i' , 'c' , 'k'];
-// var quesedilla = ['q', 'u', 'e', 's', 'e', 'd', 'i', 'l', 'l', 'a'];
-// var pigskin = ['p', 'i', 'g', 's', 'k', 'i', 'n'];
-// var liger = ['l', 'i', 'g', 'e', 'r'];
-// var skills = ['s', 'k', 'i', 'l', 'l', 's'];
-// var talons = ['t', 'a', 'l', 'o', 'n', 's'];
-// var dreams = ['d', 'r', 'e', 'a', 'm', 's'];
-// var babes = ['b', 'a', 'b', 'e', 's'];
-// var college = ['c', 'o', 'l', 'l', 'e', 'g', 'e'];
-// var tots = ['t' + 'o' + 't' + 's'];
 
 var wordBank = [
-    chapstick, 
-    quesedilla, 
-    pigskin, 
-    liger, 
-    skills, 
-    talons, 
-    dreams, 
-    babes, 
-    college,
-    tots
+    'chapstick', 
+    'quesedilla', 
+    'pigskin', 
+    'liger', 
+    'skills', 
+    'talons', 
+    'dreams', 
+    'babes', 
+    'college',
+    'tots'
 ];
 
 var randomWord = "";
@@ -34,42 +17,36 @@ var lettersOfWord = []
 var blanks = 0;
 var blanksAndCorrect = [];
 var wrongGuess = [];
+var guess = [];
 
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 
-document.onkeydown = function() {
-
-    if (start.style.display === "none") {
-        start.style.display = "none";
-    } else {
-        start.style.display = "none";
-    }
+function game() {
+    randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    lettersOfWord = randomWord.split("");
+    blanks = lettersOfWord.length;
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    };
     
-    function game() {
-        randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-        lettersOfWord = randomWord.split("");
-        blanks = lettersOfWord.length;
-        for (var i = 0; i < blanks; i++) {
-            blanksAndCorrect.push("_");
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    document.onkeyup = function letterCheck() {
+        guess = event.key.toLowerCase();
+        if(guess = lettersOfWord) {
+            console.log(guess);
+            blanksAndCorrect.push(guess);
         }
-    }
+    };
+};
 
-    // for(i = 0, i < wordBank.length, i++) {
-    //     var word = document.getElementById('word');
-    //     var nextWord = wordBank[i];
-    //     var ul = document.createElement('ul');
-    //     ul.textContent = nextWord.join('').replace(/[a-z]/g, '_');
-    //     word.appendChild(ul);
-    //     var userGuess = element.key;
-    //         if(userGuess.onkeyup) {
-    //             ul.textContent = nextWord.join('');
-    //             word.appendChild(ul);
-    //         };
-    // };
-
-    
-    
-
+window.onload = function start() {
+    var start = document.getElementById('start');
+    var div = document.createElement('div');
+    div.textContent= "Press any key to start.";
+    start.appendChild(div);
 }
+
+document.onkeydown = game()
